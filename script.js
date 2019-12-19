35cd3813ce8fccbaaff3d7c6d0c66b7f853e71d8
Otman404 <otmaneboughaba@gmail.com>
2019-12-19 17:46:13 +0100

    coloring shortest path

--- script.js
+++ script.js
@@ -40,7 +40,8 @@ function addEdge() {
       id: document.getElementById("edge-id").value,
       from: document.getElementById("edge-from").value,
       to: document.getElementById("edge-to").value,
-      capacity: document.getElementById("edge-capacity").value
+      capacity: document.getElementById("edge-capacity").value,
+      label: document.getElementById("edge-capacity").value
     });
   } catch (err) {
     alert(err);
@@ -51,12 +52,15 @@ function updateEdge() {
     edges.update({
       id: document.getElementById("edge-id").value,
       from: document.getElementById("edge-from").value,
-      to: document.getElementById("edge-to").value
+      to: document.getElementById("edge-to").value,
+      capacity: document.getElementById("edge-capacity").value,
+      label: document.getElementById("edge-capacity").value,
     });
   } catch (err) {
     alert(err);
   }
 }
+
 function removeEdge() {
   try {
     edges.remove({ id: document.getElementById("edge-id").value });
@@ -68,7 +72,7 @@ function removeEdge() {
 function draw() {
   // create an array with nodes
   nodes = new vis.DataSet();
-  nodes.on("*", function() {
+  nodes.on("*", function () {
     document.getElementById("nodes").innerHTML = JSON.stringify(
       nodes.get(),
       null,
@@ -76,16 +80,17 @@ function draw() {
     );
   });
   nodes.add([
-    { id: "1", label: "A" },
-    { id: "2", label: "Node 2" },
-    { id: "3", label: "Node 3" }
-    // { id: "4", label: "Node 4" },
-    // { id: "5", label: "Node 5" }
+    { id: "A", label: "A" },
+    { id: "B", label: "B" },
+    { id: "C", label: "C" },
+    { id: "D", label: "D" },
+    { id: "E", label: "E" },
+    { id: "F", label: "F" },
   ]);
 
   // create an array with edges
   edges = new vis.DataSet();
-  edges.on("*", function() {
+  edges.on("*", function () {
     document.getElementById("edges").innerHTML = JSON.stringify(
       edges.get(),
       null,
@@ -93,12 +98,23 @@ function draw() {
     );
   });
   var myRedColor = "red";
+  // var capacity = 10;
   edges.add([
-    { id: "1", from: "1", to: "2",capacity:10,color: {highlight: myRedColor} },
-    { id: "2", from: "1", to: "3",capacity:5,color: {highlight: myRedColor} }
+    { id: "1", from: "A", to: "B", capacity: 7, label: "7" },
+    { id: "2", from: "A", to: "C", capacity: 9, label: "9" },
+    { id: "3", from: "A", to: "F", capacity: 14, label: "14" },
+    { id: "4", from: "B", to: "C", capacity: 10, label: "10" },
+    { id: "5", from: "B", to: "D", capacity: 15, label: "15" },
+    { id: "6", from: "C", to: "D", capacity: 11, label: "11" },
+    { id: "7", from: "C", to: "F", capacity: 2, label: "2" },
+    { id: "8", from: "D", to: "E", capacity: 6, label: "6" },
+    { id: "9", from: "E", to: "F", capacity: 9, label: "9" }
+    // { id: "9", from: "E", to: "F", capacity: 9, color: { highlight: myRedColor }, label: "9" },
+
     // { id: "2", from: "1", to: "3" },
     // { id: "3", from: "2", to: "4" },
     // { id: "4", from: "2", to: "5" }
+
   ]);
 
   // create a network
@@ -111,21 +127,144 @@ function draw() {
   network = new vis.Network(container, data, options);
 }
 
-var gNodes = JSON.parse(!document.getElementById("nodes").innerHTML);
-var gEdges = JSON.parse(!document.getElementById("edges").innerHTML);
 
-// for i .. gEdges.length
-//     for j .. gEdges.id.lengh
-//var b = gEdges.id[i]:{gEdges.to[i]};+var;
 
-const graph = {
-  start: {A: 5, B: 2},
-  A: {C: 4, D: 2},
-  B: {A: 8, D: 7},
-  C: {D: 6, finish: 3},
-  D: {finish: 1},
-  finish: {}
-};
+
+
+
+
+  const dijkstra = (edges, source, target) => {
+    const Q = new Set(),
+      prev = {},
+      dist = {},
+      adj = {}
+
+    const vertex_with_min_dist = (Q, dist) => {
+      let min_distance = Infinity,
+        u = null
+
+      for (let v of Q) {
+        if (dist[v] < min_distance) {
+          min_distance = dist[v]
+          u = v
+        }
+      }
+      return u
+    }
+
+    for (let i = 0; i < edges.length; i++) {
+      let v1 = edges[i][0],
+        v2 = edges[i][1],
+        len = edges[i][2]
+
+      Q.add(v1)
+      Q.add(v2)
+
+      dist[v1] = Infinity
+      dist[v2] = Infinity
+
+      if (adj[v1] === undefined) adj[v1] = {}
+      if (adj[v2] === undefined) adj[v2] = {}
+
+      adj[v1][v2] = len
+      adj[v2][v1] = len
+    }
+
+    dist[source] = 0
+
+    while (Q.size) {
+      let u = vertex_with_min_dist(Q, dist),
+        neighbors = Object.keys(adj[u]).filter(v => Q.has(v)) //Neighbor still in Q 
+
+      Q.delete(u)
+
+      if (u === target) break //Break when the target has been found
+
+      for (let v of neighbors) {
+        let alt = dist[u] + adj[u][v]
+        if (alt < dist[v]) {
+          dist[v] = alt
+          prev[v] = u
+        }
+      }
+    }
+
+    {
+      let u = target,
+        S = [u],
+        len = 0
+
+      while (prev[u] !== undefined) {
+        S.unshift(prev[u])
+        len += adj[u][prev[u]]
+        u = prev[u]
+      }
+      return [S, len]
+    }
+  }
+
+
+
+
+  function updateEdge1(id,from,to,capacity) {
+    try {
+      edges.update({
+        id: id,
+        from: from,
+        to: to,
+        capacity: capacity,
+        label: capacity,
+        // color: { highlight: "red"},
+        font : {
+          enabled: true,
+          size: 25,
+          face: 'solid',
+          color: 'black',
+        },
+        background: {
+          enabled: true,
+          size:5,
+          color: "rgba(32, 191, 107,1.0)"
+        }
+      });
+    } catch (err) {
+      alert(err);
+    }
+  }
+
+function getShortestPath(){
+  var gNodes = document.getElementById("nodes").innerHTML;
+  var gEdges = document.getElementById("edges").innerHTML;
+
+  if (gNodes.length!=0 && gEdges.length!=0){
+     gNodes = JSON.parse(document.getElementById("nodes").innerHTML);
+     gEdges = JSON.parse(document.getElementById("edges").innerHTML);
+     
+     //Testing algorithm
+     let graph = []
+     for(var i = 0;i<gEdges.length;i++){
+       graph.push([gEdges[i].from, gEdges[i].to, parseInt(gEdges[i].capacity)]);
+       
+      }
+      
+      let [path, length] = dijkstra(graph, gNodes[0].id , gNodes[gNodes.length - 2].id);
+      var j=0;
+      for(var i=0;i<gEdges.length;i++){
+ 
+        if((gEdges[i].from === path[j]) && (gEdges[i].to === path[j+1])){
+          updateEdge1(gEdges[i].id,gEdges[i].from,gEdges[i].to,gEdges[i].capacity);
+          if(j<path.length) j++;
+          console.log("INSIDE THE LOOP");
+        }
+      }
+      
+      
+      console.log(path) //[ 'a', 'c', 'f', 'e' ]
+      console.log(length) //20
+    }
+      
+    }
+    
 
 
 window.addEventListener("load", () => {
